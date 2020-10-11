ae83f0271bad92340e2a032578a230524c8fb53a
Elizabeth Chin <elizabethtchin@gmail.com>
2020-10-11 12:00:50 -0700

    add scaffold, appbar and body widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,15 @@ void main(){
 }
 
 class MyApp extends StatelessWidget {
+  @override //code below overrides StatelessWidget's build method, this is cleaner
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),); 
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App") ,),
+        body: Text("This is my default text!"),
+      ),
+    ); 
   }
 }
+ 
